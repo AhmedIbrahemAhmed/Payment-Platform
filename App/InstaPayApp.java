43195dd1d9597be7f4d6a63321c2421f17ca12da
Body811 <bodyashraf500@gmail.com>
2023-11-14 23:36:02 +0200

    commit numero cuatro

--- App/InstaPayApp.java
+++ App/InstaPayApp.java
@@ -1,10 +1,13 @@
 package App;
 
+import Account.InstaPayAccount;
+import TransactionProcessing.Transfer;
+import Provider.*;
 import java.util.Scanner;
 
 public class InstapayApp {
     private final Scanner scan = new Scanner(System.in);
-//    private InstapayAccount account;
+
     public void run(){
     boolean loop = true;
         while (loop) {
@@ -16,42 +19,40 @@ public class InstapayApp {
             System.out.print("-->");
             int choice = Integer.parseInt(scan.nextLine());
             switch (choice) {
-
-                case 1:
-                    doRegistration();
-                    break;
-                case 2:
-                    doLogin();
-                    break;
-                case 3:
-                    loop = false;
-                    break;
-                default:
-                    System.out.println("Invalid choice");
+                case 1 -> registerInterface();
+                case 2 -> loginInterface();
+                case 3 ->  loop = false;
+                default -> System.out.println("Invalid choice");
             }
         }
+        scan.close();
     }
 
-    private void doRegistration() {
-        Instapayaccount account = null;
+    private void registerInterface() {
+        boolean registered = false;
         do {
             try {
-                System.out.print("Enter username: ");
+                System.out.print("Enter username (has to be unique): ");
                 String username = scan.nextLine();
                 System.out.print("Enter password: ");
                 String password = scan.nextLine();
+                System.out.print("Enter your display name: ");
+                String fullName = scan.nextLine();
                 System.out.print("Enter your phone number: ");
                 String phoneNum = scan.nextLine();
-                System.out.print("Your phone number is registered with:\n1. Bank\n2. Online Wallet\n-->");
+                System.out.println("Your phone number is registered with:");
+                System.out.println("1.Bank");
+                System.out.println("2.Online Wallet");
+                System.out.println("-->");
                 int accountType = Integer.parseInt(scan.nextLine());
 
                 Signup signup = new Signup();
                 switch (accountType) {
                     case 1:
-                        account = signup.register(username, password, phoneNum, "Bank");
+                        registered = signup.register(username, password, fullName, "Bank", phoneNum);
                         break;
                     case 2:
-                        account = signup.register(username, password, phoneNum, "Wallet");
+                         registered = signup.register(username, password, fullName, "Wallet", phoneNum);
                         break;
                     default:
                         System.out.println("Invalid choice!");
@@ -60,12 +61,11 @@ public class InstapayApp {
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
             }
-        } while (account == null);
-        mainInterface(account);
+        } while (!registered);
     }
 
-    private void doLogin() {
-        Instapayaccount account = null;
+    private void loginInterface() {
+        InstaPayAccount account = null;
         do {
             try {
                 System.out.print("Enter username: ");
@@ -74,7 +74,7 @@ public class InstapayApp {
                 String password = scan.nextLine();
 
                 Login login = new Login();
-                account = login.loginfunc(username, password);
+                account = login.login(username, password);
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
             }
@@ -83,12 +83,41 @@ public class InstapayApp {
     }
 
 
-    private void mainInterface(InstapayAccount account){
-        System.out.println("------Welcome "+account.getname()+" To InstaPay------\n\n");
-        System.out.println("Choose One Of The Following Options:");
-        System.out.println("1.Tranfer Money");
-        System.out.println("2.Pay Bills");
+    private void mainInterface(InstaPayAccount account){
+        boolean loop = true;
+        System.out.println("------Welcome "+account.getFullName()+" To InstaPay------\n\n");
+        while (loop){
+            System.out.println("Choose One Of The Following Options:");
+            System.out.println("1.Make a Transaction");
+            System.out.println("2.Display balance");
+            System.out.println("3.logout");
+            System.out.print("-->");
+            int choice = Integer.parseInt(scan.nextLine());
 
+            switch(choice){
+                case 1:
+                    Transfer transfer = new Transfer(account);
+                    transfer.transfer();
+                    break;
+
+                case 2:
+                    Provider provider;
+                    double balance;
+                    if (account.getAccountType().equalsIgnoreCase("bank"))
+                        provider = new Bank();
+                    else
+                        provider = new Wallet();
+                    balance = provider.getBalance(account.getPhoneNumber());
+                    System.out.println(account.getFullName()+"'s balance: "+balance);
+                    break;
+                case 3:
+                    System.out.println("Logged out successfully. Goodbye!");
+                    loop = false;
+                default:
+                    System.out.println("Invalid choice!");
+                    break;
+            }
+        }
     }
 }
 
